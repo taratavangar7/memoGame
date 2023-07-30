a43657ba090405ef015ec7e9577258aa4985dbd3
divinetara777 <125673326+taratavangar7@users.noreply.github.com>
2023-07-30 23:45:02 +0330

    JS Update
    added carts, and flip cards

--- app.js
+++ app.js
@@ -1,11 +1,102 @@
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => {
 
-const cardArray = [
-    {
-        name: 'fries'
-       
+    // cards
+    const cardArray = [{
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+
+    ]
+
+
+
+    const grid = document.querySelector('.grid')
+    let cardChosen = []
+
+    // create board
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            let card = document.createElement('img')
+            card.setAttribute('src', 'images/blank.png')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click' , flipCard)
+            grid.appendChild(card)
+        }
     }
-]
+
+
+// check for matches
+
+
+
+// flip your card
+function flipCard() {
+    let cardId = this.getAttribute('data-id')
+    cardChosen.push(cardArray)
+
+}
+
+
+
+
+
+
+
+
+
+
+    createBoard()
+
+
+
+
+
+
+
 
 
 
